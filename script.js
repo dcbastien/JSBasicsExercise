01a9ca59590f8272a9a684db17701869760d8be7
dcbastien <ddcbastien@gmail.com>
2021-09-20 22:25:19 -0500

    Completed bonus exercise

--- script.js
+++ script.js
@@ -32,5 +32,27 @@ console.log("Deborah".length + "Bastien".length);
 console.log("Avengers: Endgame" [16]);
 
 // BONUS
-// 1
-console.log();
+// 1 - ?
+console.log("\\");
+
+// 2
+console.log(5**4-9);
+
+// 3
+console.log(("I".length + "Am".length) * "Iron".length % "Man".length);
+
+// 4 
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+// 5
+var m=25
+var c=4
+var u= m + c
+console.log(u);
