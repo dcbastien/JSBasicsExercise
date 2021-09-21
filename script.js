8155824ca2b1ed470bf0fbed65430483345c4e03
dcbastien <ddcbastien@gmail.com>
2021-09-20 21:51:53 -0500

    Completed Exercise no Bonus

---
+++ script.js
@@ -0,0 +1,36 @@
+// 1
+console.log(47);
+// 2 - 5 Primitives
+/** 1.Numbers
+ * 2. Strings
+ * 3. Boolean
+ * 4. Undefined
+ * 5. Null */
+// 3 - 6 Operators
+/** 1. Addition
+ *  2. Subtraction
+ *  3. Multiplication
+ *  4. Division
+ *  5. Exponentiation
+ *  6. Modulus
+ */
+
+// 4 
+console.log(5%4);
+console.log(5%2);
+
+// 5
+console.log('I\'M MARY POPPINS, Y\'ALL');
+
+// 6
+console.log("Hawk" + "eye");
+
+// 7
+console.log("Deborah".length + "Bastien".length);
+
+// 8
+console.log("Avengers: Endgame" [16]);
+
+// BONUS
+// 1
+console.log();
